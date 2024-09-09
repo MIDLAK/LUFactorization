36c552ef68ba46969f4812f999d80ea54495fba4
Vadim <kalugavadim04@gmail.com>
2024-09-09 11:59:57 +0700

    start

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.0.10"
+    application
+}
+
+group = "org.vadim"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("org.vadim.MainKt")
+}
